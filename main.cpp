8f6b1d5f090775a605585d96b02da99394ce6eb5
Yashal Shakti <shaktiyashal@gmail.com>
2016-05-17 14:05:48 +0530

    Add welcome inst

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include "main.h"
 #include "ShortestPath/ShortestPath.h"
 int main(int argc, char *argv[]) {
+  system(" zenity --info --title=\"Visualizing shortest path algorithms\""
+             " --text=\"Click to select a source,"
+             " destination and then draw obstruction\n"
+             "\nPress middle button to start the visualization\""
+             "\nPress OK to continue");
   ShortestPath *shortestPath = new ShortestPath();
   shortestPath->main(argc, argv);
 }
