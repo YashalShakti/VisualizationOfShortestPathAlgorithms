077478c2deab4d06974228fd75be116c04fd0c47
Yashal Shakti <shaktiyashal@gmail.com>
2016-05-18 11:25:37 +0530

    Update message

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ int main(int argc, char *argv[]) {
   system(" zenity --info --title=\"Visualizing shortest path algorithms\""
              " --text=\"Click to select a source,"
              " destination and then draw obstacles\n"
-             "\nPress middle button/z to toggle the states\""
-             "\nA: Running A* only\""
-             "\nD: Running Djikstras' only\""
-             "\nPress OK to continue");
+             "\nPress middle button/z to toggle the states\n"
+             "\nA: Running A* only\n"
+             "\nD: Running Djikstras' only\n"
+             "\nPress OK to continue\"");
   ShortestPath *shortestPath = new ShortestPath();
   shortestPath->main(argc, argv);
 }
